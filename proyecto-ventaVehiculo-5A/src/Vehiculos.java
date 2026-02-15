128b2f9ed549852e4090eb4ca4535f8e2a886769
avazquez25400596 <anjuvazquezan@ittepic.edu.mx>
2026-02-14 23:27:08 -0700

    feat: actualizacion nombre clase Vehiculos

---
+++ proyecto-ventaVehiculo-5A/src/Vehiculos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ajva_
+ */
+public class Vehiculos {
+    private String marca;
+    private double precio;
+    private String motor;
+    
+    public Vehiculos(String marca, double precio, String motor)
+    {
+        this.marca = marca;
+        this.precio= precio;
+        this.motor=motor;
+    }
+    @Override
+    public String toString(){
+        return "marca="+this.marca+"\n";
+        return "precio="+this.precio+"\n";
+        return "motor="+this.motor+"\n";
+
+    }
+    }
+    
+    
+}
